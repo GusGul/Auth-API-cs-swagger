8c86a0d11b1ec45792c6f4b286a4dcd271d44ca0
Gustavo Salgado Lima <gustavosalgadolima@gmail.com>
2023-01-13 20:18:31 -0300

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth_API_1.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+    }
+}
